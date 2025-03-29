986d4789ab1215059e3573aa8235ce5a3e00690a
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-29 15:04:39 +0800

    新增组件扫描包配置

--- api-platform-java/api-backend/src/main/java/com/qfleaf/yunapi/YunApiApplication.java
+++ api-platform-java/api-backend/src/main/java/com/qfleaf/yunapi/YunApiApplication.java
@@ -3,9 +3,11 @@ package com.qfleaf.yunapi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.qfleaf.yunapi.mapper")
+@ComponentScan({"com.qfleaf.yunapi.**", "com.qfleaf.web.*"})
 public class YunApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(YunApiApplication.class, args);
