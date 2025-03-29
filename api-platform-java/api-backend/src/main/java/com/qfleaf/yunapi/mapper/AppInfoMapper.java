58e4441a3c26aa8603feccb461c4e1daefd4cca3
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-29 17:27:49 +0800

    实现自定义根据id查询视图、根据条件构造分页查询视图

--- api-platform-java/api-backend/src/main/java/com/qfleaf/yunapi/mapper/AppInfoMapper.java
+++ api-platform-java/api-backend/src/main/java/com/qfleaf/yunapi/mapper/AppInfoMapper.java
@@ -1,7 +1,13 @@
 package com.qfleaf.yunapi.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.qfleaf.yunapi.entity.AppInfo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qfleaf.yunapi.open.model.vo.AppInfoPageVO;
+import com.qfleaf.yunapi.open.model.vo.AppInfoVO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
 * @author qianfang
@@ -10,7 +16,16 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @Entity com.qfleaf.yunapi.entity.AppInfo
 */
 public interface AppInfoMapper extends BaseMapper<AppInfo> {
-
+    @Select("select id, name, created_at " +
+            "from app_info " +
+            "${ew.customSqlSegment}")
+    IPage<AppInfoPageVO> selectPageVO(IPage<AppInfoPageVO> page, @Param("ew") LambdaQueryWrapper<AppInfo> queryWrapper);
+    @Select("select t1.id, username as developer, name, description, t1.created_at " +
+            "from app_info t1 " +
+            "left join users t2 " +
+            "on t2.id = t1.user_id " +
+            "where t1.id = ${id}")
+    AppInfoVO selectVoById(long id);
 }
 
 
