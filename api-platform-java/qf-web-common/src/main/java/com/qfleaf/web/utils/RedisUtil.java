07853396b794163b0d2080ff0e9383c5f0dbdc7b
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-29 15:00:30 +0800

    优化imports

--- api-platform-java/qf-web-common/src/main/java/com/qfleaf/web/utils/RedisUtil.java
+++ api-platform-java/qf-web-common/src/main/java/com/qfleaf/web/utils/RedisUtil.java
@@ -1,11 +1,6 @@
 package com.qfleaf.web.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.ListOperations;
-import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
