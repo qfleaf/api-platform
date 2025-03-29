8f5dfa3e765a4d6b9d40bda087d8b9edeb94994d
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-29 15:03:10 +0800

    实现用户登陆、登出、查找当前用户信息的接口

---
+++ api-platform-java/api-backend/src/main/java/com/qfleaf/yunapi/controller/UsersController.java
@@ -0,0 +1,39 @@
+package com.qfleaf.yunapi.controller;
+
+import com.qfleaf.web.common.CommonResponse;
+import com.qfleaf.web.common.ResponseCode;
+import com.qfleaf.web.utils.ResultUtil;
+import com.qfleaf.yunapi.open.model.dto.UserLoginRequest;
+import com.qfleaf.yunapi.open.model.vo.LoginUserVO;
+import com.qfleaf.yunapi.open.model.vo.UserLoginResponse;
+import com.qfleaf.yunapi.service.UsersService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("user")
+public class UsersController {
+    private final UsersService usersService;
+
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @GetMapping("current")
+    public CommonResponse<LoginUserVO> getCurrentUser() {
+        LoginUserVO loginUserVO = usersService.getCurrentUser();
+        return ResultUtil.success(ResponseCode.SUCCESS, loginUserVO);
+    }
+
+    @PostMapping("login")
+    public CommonResponse<UserLoginResponse> login(@Valid @RequestBody UserLoginRequest userLoginRequest) {
+        UserLoginResponse result = usersService.login(userLoginRequest);
+        return ResultUtil.success(ResponseCode.SUCCESS_LOGIN, result);
+    }
+
+    @PostMapping("logout")
+    public CommonResponse<Void> logout() {
+        usersService.logout();
+        return ResultUtil.success(ResponseCode.SUCCESS_LOGIN, null);
+    }
+}
