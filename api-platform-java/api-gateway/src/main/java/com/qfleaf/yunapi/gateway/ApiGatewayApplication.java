a2724d11255deee17248b274c7380fe3e09ba30b
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-31 10:13:54 +0800

    添加启动配置

--- api-platform-java/api-gateway/src/main/java/com/qfleaf/yunapi/gateway/ApiGatewayApplication.java
+++ api-platform-java/api-gateway/src/main/java/com/qfleaf/yunapi/gateway/ApiGatewayApplication.java
@@ -1,9 +1,13 @@
 package com.qfleaf.yunapi.gateway;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class, DataSourceAutoConfiguration.class})
+@EnableDubbo
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
