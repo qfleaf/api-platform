c2384ed6711d718c25c719797431811b14289952
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-31 15:20:43 +0800

    开启redis自动配置、提供访问频率限制器的ipKeyResolver

--- api-platform-java/api-gateway/src/main/java/com/qfleaf/yunapi/gateway/ApiGatewayApplication.java
+++ api-platform-java/api-gateway/src/main/java/com/qfleaf/yunapi/gateway/ApiGatewayApplication.java
@@ -3,13 +3,20 @@ package com.qfleaf.yunapi.gateway;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
-@SpringBootApplication(exclude = {RedisAutoConfiguration.class, DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDubbo
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
     }
 }
