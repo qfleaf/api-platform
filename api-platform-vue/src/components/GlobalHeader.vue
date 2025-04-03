d7cb565a78b61759aaf1dfca0a96d0313bc85508
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-04-03 14:08:53 +0800

    实现登陆登出功能

--- api-platform-vue/src/components/GlobalHeader.vue
+++ api-platform-vue/src/components/GlobalHeader.vue
@@ -1,22 +1,17 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { h, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { UserOutlined } from '@ant-design/icons-vue';
+import { useUserStore } from '../stores/userStore';
 
-// const searchQuery = ref('');
 const router = useRouter();
-const isLoggedIn = ref(false); // 模拟登录状态
-
-// const handleSearch = () => {
-//   if (searchQuery.value.trim()) {
-//     router.push({ path: '/search', query: { q: searchQuery.value } });
-//   }
-// };
+const userStore = useUserStore();
+const isLoggedIn = ref<boolean>(userStore.isLogin);
 
 // 退出登录
 const handleLogout = () => {
+  userStore.logout();
   isLoggedIn.value = false;
-  router.push('/login');
 };
 
 // 登录跳转
@@ -33,16 +28,13 @@ const handleLogin = () => {
       <span class="logo-text">晓芸API</span>
     </div>
 
-    <!-- 搜索框 -->
-    <!-- <a-input-search v-model:value="searchQuery" placeholder="搜索接口..." class="search-box" @search="handleSearch" /> -->
-
     <!-- 右侧 用户操作 -->
     <div class="user-actions">
       <a-button v-if="!isLoggedIn" type="primary" class="login-btn" @click="handleLogin">登录</a-button>
 
       <a-dropdown v-else>
         <a class="user-avatar">
-          <a-avatar size="large" :icon="UserOutlined" />
+          <a-avatar size="large" :icon="h(UserOutlined)" />
         </a>
         <template #overlay>
           <a-menu>
@@ -86,13 +78,6 @@ const handleLogin = () => {
   font-size: 22px;
   font-weight: bold;
   color: white;
-}
-
-/* 搜索框 */
-.search-box {
-  flex: 1;
-  max-width: 400px;
-  margin: 0 20px;
 }
 
 /* 用户操作 */
