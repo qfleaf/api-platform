4d2fa1292ce344d64e4024edcdf412752d974858
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-04-03 11:21:38 +0800

    提供一个拼接query参数的工具类

---
+++ api-platform-java/yunapi-sdk/src/main/java/com/qfleaf/yunapi/sdk/QueryParamUtil.java
@@ -0,0 +1,12 @@
+package com.qfleaf.yunapi.sdk;
+
+import org.springframework.web.util.UriComponentsBuilder;
+import java.util.Map;
+
+public final class QueryParamUtil {
+    public static String mapToQueryParams(String baseUrl, Map<String, Object> params) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUrl);
+        params.forEach(builder::queryParam);
+        return builder.toUriString();
+    }
+}
