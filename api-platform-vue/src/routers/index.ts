6d5f321695cb5eee727f154c11bc62ed08402c3f
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-04-09 09:26:45 +0800

    提供接口详情页、实现在线调试功能

--- api-platform-vue/src/routers/index.ts
+++ api-platform-vue/src/routers/index.ts
@@ -3,6 +3,8 @@ import { createWebHistory, createRouter } from 'vue-router'
 const routes = [
     { path: '/', component: () => import('../pages/HomePage.vue') },
     { path: '/login', component: () => import('../pages/LoginPage.vue') },
+    
+    { path: '/api/info', component: () => import('../pages/ApiInfoPage.vue') },
 ]
 
 const router = createRouter({
